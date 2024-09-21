6b7ec3432b122fef6b166429a196e2f39b276c78
Thanarat Ruengkum <ninenights19534@gmail.com>
2024-09-22 03:06:06 +0700

    Day 3 22/09/2567

---
+++ 0006.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    double r,pi=3.141592653;
+    cin >> r;
+    printf("%.6lf\n%.6lf",M_PI*(r*r),2*r*r);
+}
---
+++ 0007.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x1,s;
+    cin >> x1 >> s;
+    cout << s*2-x1;
+}
